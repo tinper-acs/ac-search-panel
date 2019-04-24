35f9c140348cbad7dd402522159be2af7a57118a
liushaozhen <804199994@qq.com>
2019-04-24 15:23:47 +0800

    1.0.0版本 基于bee-search-panel封装

--- demo/demolist/Demo1.js
+++ demo/demolist/Demo1.js
@@ -6,78 +6,229 @@
  */
 import React, {Component} from 'react';
 import SearchPanel from '../../src';
-import {FormControl,Row, Col,Label,FormGroup,Radio,Button} from 'tinper-bee';
+import { Label,Checkbox,Switch,Button, Radio, Select,  Col , Row , FormControl,Form } from 'tinper-bee';
+import DatePicker from 'bee-datepicker';
+import moment from "moment/moment";
+const { RangePicker } = DatePicker;
+const FormItem = Form.FormItem;
+const Option = Select.Option;
+const CheckboxGroup = Checkbox.CheckboxGroup;
+const HeadContainer = SearchPanel.HeadContainer;
+const AdvancedContainer = SearchPanel.AdvancedContainer;
 
-
-class Demo1 extends Component {
-    constructor(props){
+class Demo extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            state:'all',
-        }
+        this.state = {
+            expanded: true,
+            orderTypes:[
+                {
+                    'code':'001',
+                    'name':'类型1'
+                },
+                {
+                    'code':'002',
+                    'name':'类型2'
+                },
+                {
+                    'code':'003',
+                    'name':'类型3'
+                },
+            ]
+        };
     }
-    stateChange(value){
+    submit = (e) => {
+        this.props.form.validateFields((err, values) => {
+            if (err) {
+                console.log('校验失败', values);
+            } else {
+                console.log('提交成功', values)
+            }
+        });
+    }
+    reset = () =>{
+        this.props.form.resetFields();
+        //部分表单元素无法通过this.props.form.resetFields重置，需要手动重置，如下
         this.setState({
-            state:value
+            approvalState: '',
+            closeState: '',
+            confirmState: '',
+            voucherDate: []
         })
     }
-    typeChange(value){
-        this.setState({
-            type:value
-        })
+    onChange = () => {
+        this.setState({expanded: !this.state.expanded})
     }
-    degreeChange(value){
-        this.setState({
-            degree:value
-        })
-    }
-    search(){
 
+    search =()=>{
+        this.props.form.validateFields((err, values) => {
+            if (err) {
+                console.log('校验失败', values);
+            } else {
+                console.log('提交成功', values)
+            }
+        });
     }
-    clear(){
 
-    }
     render() {
-        let searchContent=()=>{
-            return (
-                <div className="demo">
-                    <div>
-                        <label className="demo-label">状态:</label>
-                        <Radio.RadioGroup
-                            name="state"
-                            selectedValue={this.state.state}
-                            onChange={this.stateChange.bind(this)}>
-                            <Radio.RadioButton value="all">全部</Radio.RadioButton>
-                            <Radio.RadioButton value="initial">初始化</Radio.RadioButton>
-                            <Radio.RadioButton value="todo">待处理</Radio.RadioButton>
-                            <Radio.RadioButton value="doing">处理中</Radio.RadioButton>
-                            <Radio.RadioButton  value="done">已完成</Radio.RadioButton>
-                            <Radio.RadioButton  value="closed">已完成</Radio.RadioButton>
-                        </Radio.RadioGroup>
-                    </div>
-
-                    <div className="margin-top-10">
-                        <span className="demo-item">
-                            <label className="demo-label">名称:</label>
-                            <FormControl placeholder="请输入名称"/>
-                        </span>
-                        <span className="demo-item">
-                            <label className="demo-label">编码:</label>
-                            <FormControl placeholder="请输入编码"/>
-                        </span>
-                    </div>
-                </div>
-            )
-        }
-        return (
+        const { getFieldProps, getFieldError } = this.props.form;
+        let self = this;
+        
+        return (<div className="demo">
             <SearchPanel
-                searchHead='基础示例'
-                searchContent={searchContent()}
-                searchClick={this.search}
-                clearClick={this.clear}
-                searchOpen={true}
-            />
+                title='条件筛选'
+                onSearch={this.search}
+                onReset={this.reset}
+                expanded={this.state.expanded}
+                onChange={this.onChange}
+                onPanelChangeStart={status => {
+                    console.log(status, "start")
+                }}
+                onPanelChangeIng={status => {
+                    console.log(status, "ing")
+                }}
+                onPanelChangeEnd={status => {
+                    console.log(status, "end")
+                }}>
+                <HeadContainer>
+                    <div className='demo-head'>
+                        <Form>
+                            <Row>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <FormItem>
+                                            <Label>订单编号</Label>
+                                            <FormControl size="sm"
+                                                {
+                                                ...getFieldProps('orderCode', {
+                                                    initialValue: '',
+                                                })
+                                                }
+                                            />
+                                    </FormItem>
+                                </Col>
+
+                                <Col xs={12} sm={6} md={4}  lg={3}>
+                                    <FormItem>
+                                            <Label>供应商名称</Label>
+                                            <FormControl size="sm"
+                                                {
+                                                ...getFieldProps('supplierName', {
+                                                    initialValue: '',
+                                                })
+                                                }
+                                            />
+                                    </FormItem>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </div>
+                </HeadContainer>
+                <AdvancedContainer>
+                    <div className='demo-body'>
+                        <Form>
+                            <Row>
+                                <Col xs={12} sm={6} md={4}  lg={3}>
+                                    <FormItem>
+                                            <Label>订单类型</Label>
+                                            <Select size="sm"
+                                                {
+                                                ...getFieldProps('type', {
+                                                    initialValue: '',
+                                                }
+                                                )}>
+                                                <Option value="">请选择</Option>
+                                                {
+                                                    self.state.orderTypes.map((item, index) => {
+                                                        return (
+                                                            <Option key={index} value={item.code}>{item.name}</Option>
+                                                        )
+                                                    })
+                                                }
+                                            </Select>
+                                    </FormItem>
+                                </Col>
+
+                                <Col xs={12} sm={6} md={4}  lg={3}>
+                                    <FormItem>
+                                            <Label>采购组</Label>
+                                            <CheckboxGroup 
+                                                    {
+                                                        ...getFieldProps('purchasingGroup',{
+                                                            initialValue:['2']
+                                                        })
+                                                    }
+                                                >
+                                                    <Checkbox value='1'>人力</Checkbox>
+                                                    <Checkbox value='2'>财务</Checkbox>
+                                            </CheckboxGroup>
+                                    </FormItem>
+                                </Col>
+
+                                <Col xs={12} sm={6} md={4}  lg={3}>
+                                    <FormItem>
+                                        {/* <Col xs={12} sm={12} md={12}  lg={12} className="col"> */}
+                                            <Label>审批</Label>
+                                            <Radio.RadioGroup
+                                                    selectedValue={this.state.approvalState}
+                                                    {
+                                                    ...getFieldProps('approvalState', {
+                                                        initialValue: '1',
+                                                        onChange(value) {
+                                                            self.setState({ approvalState: value });
+                                                        },
+                                                    }
+                                                    )}
+                                                >
+                                                <Radio value="0" >未审批</Radio>
+                                                <Radio value="1" >已审批</Radio>
+                                            </Radio.RadioGroup>
+                                        {/* </Col> */}
+                                    </FormItem>
+                                </Col>
+                                <Col xs={12} sm={6} md={4}  lg={3}>
+                                    <FormItem>
+                                            <Label className='bottom-10'>创建时间</Label>
+                                            <DatePicker  format = "YYYY-MM-DD"
+                                                {
+                                                ...getFieldProps('time',
+                                                    {
+                                                        initialValue: '',
+                                                    }
+                                                )}/>
+                                    </FormItem>
+                                </Col>
+                                <Col xs={12} sm={6} md={4}  lg={3}>
+                                    <FormItem>
+                                            <Label className='bottom-10'>起止日期</Label>
+                                            <RangePicker  placeholder={'开始 ~ 结束'}
+                                                {
+                                                ...getFieldProps('times',
+                                                    {
+                                                        initialValue: [],
+                                                    }
+                                                )}/>
+                                    </FormItem>
+                                </Col>
+                                <Col xs={12} sm={6} md={4}  lg={3}>
+                                    <FormItem>
+                                            <Label>是否启用</Label>
+                                            <Switch 
+                                                {
+                                                ...getFieldProps('enable',
+                                                    {
+                                                        initialValue: false,
+                                                    }
+                                                )}/>
+                                    </FormItem>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </div>
+                </AdvancedContainer>
+            </SearchPanel>
+            </div>
         )
     }
 }
-export default Demo1;
+
+export default Form.createForm()(Demo);
--- demo/index.js
+++ demo/index.js
@@ -1,21 +1,15 @@
-
-import { Con, Row, Col } from 'bee-layout';
-import { Panel } from 'bee-panel';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Button from 'bee-button';
-import './demo.scss';
+import { Con, Row, Col } from 'bee-layout';
+import { Panel } from 'bee-panel';
+import Drawer from 'bee-drawer';
+import Clipboard from 'bee-clipboard'; 
+import './demo.scss'
 
-
-
-
-const CARET = <i className="uf uf-arrow-down"></i>;
-
-const CARETUP = <i className="uf uf-arrow-up"></i>;
 
 
 import Demo1 from "./demolist/Demo1";
-var DemoArray = [{"example":<Demo1 />,"title":" 应用组件名称","code":"/**\n *\n * @title 应用组件名称\n * @description 应用组件描述\n *\n */\n\nimport React, { Component } from 'react';\nimport Temp from '../../src/index';\nclass Demo1 extends Component {\n    render () {\n        return (\n            <div className=\"demoPadding\">\n                <Temp >tools测试代码</Temp>\n            </div>\n        )\n    }\n}\n\n\n","desc":" 应用组件描述"}]
+var DemoArray = [{"example":<Demo1 />,"title":" 基础示例","code":"/**\n *\n * @title 基础示例\n * @description 基础示例\n *\n */\nimport React, {Component} from 'react';\nimport SearchPanel from 'ac-search-panel';\nimport { Label,Checkbox,Switch,Button, Radio, Select,  Col , Row , FormControl } from 'tinper-bee';\nimport DatePicker from 'bee-datepicker';\nimport moment from \"moment/moment\";\nimport Form from 'bee-form';\nconst FormItem = Form.FormItem;\nconst Option = Select.Option;\nconst { RangePicker } = DatePicker;\nconst CheckboxGroup = Checkbox.CheckboxGroup;\nconst HeadContainer = SearchPanel.HeadContainer;\nconst AdvancedContainer = SearchPanel.AdvancedContainer;\n\nclass Demo extends Component {\n    constructor(props) {\n        super(props);\n        this.state = {\n            expanded: true,\n            approvalState: '1',\n            purchasingGroup:'2',\n            closeState: '',\n            confirmState: '',\n            voucherDate: [moment(),moment('2019-07-20')],\n            orderTypes:[\n                {\n                    'code':'001',\n                    'name':'类型1'\n                },\n                {\n                    'code':'002',\n                    'name':'类型2'\n                },\n                {\n                    'code':'003',\n                    'name':'类型3'\n                },\n            ]\n        };\n    }\n    submit = (e) => {\n        this.props.form.validateFields((err, values) => {\n            if (err) {\n                console.log('校验失败', values);\n            } else {\n                console.log('提交成功', values)\n            }\n        });\n    }\n    reset = () =>{\n        this.props.form.resetFields();\n        //部分表单元素无法通过this.props.form.resetFields重置，需要手动重置，如下\n        this.setState({\n            approvalState: '',\n            closeState: '',\n            confirmState: '',\n            voucherDate: []\n        })\n    }\n    onChange = () => {\n        this.setState({expanded: !this.state.expanded})\n    }\n\n    search =()=>{\n        this.props.form.validateFields((err, values) => {\n            if (err) {\n                console.log('校验失败', values);\n            } else {\n                console.log('提交成功', values)\n            }\n        });\n    }\n\n    render() {\n        const { getFieldProps, getFieldError } = this.props.form;\n        let self = this;\n        return (<div className=\"demo\">\n            <SearchPanel\n                title='条件筛选'\n                onSearch={this.search}\n                onReset={this.clear}\n                expanded={this.state.expanded}\n                onChange={this.onChange}\n                onPanelChangeStart={status => {\n                    console.log(status, \"start\")\n                }}\n                onPanelChangeIng={status => {\n                    console.log(status, \"ing\")\n                }}\n                onPanelChangeEnd={status => {\n                    console.log(status, \"end\")\n                }}>\n                <HeadContainer>\n                    <div className='demo-head'>\n                        <Form>\n                            <Row>\n                                <Col xs={12} sm={6} md={4} lg={3}>\n                                    <FormItem>\n                                            <Label>订单编号</Label>\n                                            <FormControl size=\"sm\"\n                                                {\n                                                ...getFieldProps('orderCode', {\n                                                    initialValue: '',\n                                                })\n                                                }\n                                            />\n                                    </FormItem>\n                                </Col>\n\n                                <Col xs={12} sm={6} md={4}  lg={3}>\n                                    <FormItem>\n                                            <Label>供应商名称</Label>\n                                            <FormControl size=\"sm\"\n                                                {\n                                                ...getFieldProps('supplierName', {\n                                                    initialValue: '',\n                                                })\n                                                }\n                                            />\n                                    </FormItem>\n                                </Col>\n                            </Row>\n                        </Form>\n                    </div>\n                </HeadContainer>\n            \n\n                <AdvancedContainer>\n                    <div className='demo-body'>\n                        <Form>\n                            <Row>\n                                <Col xs={12} sm={6} md={4}  lg={3}>\n                                    <FormItem>\n                                            <Label>订单类型</Label>\n                                            <Select size=\"sm\"\n                                                {\n                                                ...getFieldProps('type', {\n                                                    initialValue: '',\n                                                }\n                                                )}>\n                                                <Option value=\"\">请选择</Option>\n                                                {\n                                                    self.state.orderTypes.map((item, index) => {\n                                                        return (\n                                                            <Option key={index} value={item.code}>{item.name}</Option>\n                                                        )\n                                                    })\n                                                }\n                                            </Select>\n                                    </FormItem>\n                                </Col>\n\n                                <Col xs={12} sm={6} md={4}  lg={3}>\n                                    <FormItem>\n                                            <Label>采购组</Label>\n                                            <CheckboxGroup \n                                                    {\n                                                        ...getFieldProps('purchasingGroup',{\n                                                            initialValue:['2']\n                                                        })\n                                                    }\n                                                >\n                                                    <Checkbox value='1'>人力</Checkbox>\n                                                    <Checkbox value='2'>财务</Checkbox>\n                                            </CheckboxGroup>\n                                    </FormItem>\n                                </Col>\n\n                                <Col xs={12} sm={6} md={4}  lg={3}>\n                                    <FormItem>\n                                        {/* <Col xs={12} sm={12} md={12}  lg={12} className=\"col\"> */}\n                                            <Label>审批</Label>\n                                            <Radio.RadioGroup\n                                                    selectedValue={this.state.approvalState}\n                                                    {\n                                                    ...getFieldProps('approvalState', {\n                                                        initialValue: '1',\n                                                        onChange(value) {\n                                                            self.setState({ approvalState: value });\n                                                        },\n                                                    }\n                                                    )}\n                                                >\n                                                <Radio value=\"0\" >未审批</Radio>\n                                                <Radio value=\"1\" >已审批</Radio>\n                                            </Radio.RadioGroup>\n                                        {/* </Col> */}\n                                    </FormItem>\n                                </Col>\n                            </Row>\n                        </Form>\n                    </div>\n                </AdvancedContainer>\n            </SearchPanel>\n            </div>\n        )\n    }\n}\n\n","desc":" 基础示例"}]
 
 
 class Demo extends Component {
@@ -24,32 +18,48 @@ class Demo extends Component {
         this.state = {
             open: false
         }
-        this.handleClick = this.handleClick.bind(this);
     }
-    handleClick() {
+    handleClick=()=> {
         this.setState({ open: !this.state.open })
+    }
+    fCloseDrawer=()=>{
+        this.setState({
+            open: false
+        })
     }
 
     render () {
-        const { title, example, code, desc  } = this.props;
-        let caret = this.state.open ? CARETUP : CARET;
-        let text = this.state.open ? "隐藏代码" : "查看代码";
+        const { title, example, code, desc, scss_code  } = this.props;
 
-        const footer = (
-            <Button shape="block" onClick={ this.handleClick }>
-                { caret }
-                { text }
-            </Button>
+        const header = (
+            <div>
+                <p className='component-title'>{ title }</p>
+                <p>{ desc }</p>
+                <span className='component-code' onClick={this.handleClick}> 查看源码 <i className='uf uf-arrow-right'/> </span>
+            </div>
         );
         return (
-            <Col md={12} >
-                <h3>{ title }</h3>
-                <p>{ desc }</p>
-                <Panel collapsible expanded={ this.state.open } colors='bordered' header={ example } footer={footer} footerStyle = {{padding: 0}}>
-                    <pre><code className="hljs javascript">{ code }</code></pre>
-                </Panel>
-            </Col>
-        )
+            <Col md={12} id={title.trim()} className='component-demo'>
+            <Panel header={header}>
+                {example}
+            </Panel>
+           
+            <Drawer className='component-drawerc' title={title} show={this.state.open} placement='right' onClose={this.fCloseDrawer}>
+            <div className='component-code-copy'> JS代码 
+                <Clipboard action="copy" text={code}/>
+            </div>
+            <pre className="pre-js">
+                <code className="hljs javascript">{ code }</code>
+            </pre >
+            {!!scss_code ?<div className='component-code-copy copy-css'> SCSS代码 
+                <Clipboard action="copy" text={scss_code}/>
+            </div>:null }
+                { !!scss_code ? <pre className="pre-css">
+                 <code className="hljs css">{ scss_code }</code>
+                 </pre> : null }
+            </Drawer>
+        </Col>
+    )
     }
 }
 
@@ -59,16 +69,16 @@ class DemoGroup extends Component {
     }
     render () {
         return (
-                <Row>
-                    {DemoArray.map((child,index) => {
+            <Row>
+            {DemoArray.map((child,index) => {
 
-                        return (
-                            <Demo example= {child.example} title= {child.title} code= {child.code} desc= {child.desc} key= {index}/>
-                        )
+                return (
+            <Demo example= {child.example} title= {child.title} code= {child.code} scss_code= {child.scss_code} desc= {child.desc} key= {index}/>
+    )
 
-                    })}
-                </Row>
-        )
+    })}
+    </Row>
+    )
     }
 }
 
